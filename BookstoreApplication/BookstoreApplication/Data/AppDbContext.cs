c113637e64b310ba93472168b3ca880b37a2432b
Ilija0020 <prekogacic@gmail.com>
2026-05-03 11:09:17 +0200

    feat: implement postgre database with settings

---
+++ BookstoreApplication/BookstoreApplication/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BookstoreApplication.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookstoreApplication.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+    }
+}
--- BookstoreApplication/BookstoreApplication/Program.cs
+++ BookstoreApplication/BookstoreApplication/Program.cs
@@ -1,3 +1,6 @@
+using BookstoreApplication.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 builder.Services.AddCors(options =>
 {
@@ -19,6 +26,8 @@ builder.Services.AddCors(options =>
 });
 
 var app = builder.Build();
+
+app.UseCors("AllowAll");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
