d79a496ca97b614eeda9e6cab534b93a8f9ac0e8
Ilija0020 <prekogacic@gmail.com>
2026-05-06 10:07:35 +0200

    feat: add Award entity and AuthorAward many-to-many relationship

--- BookstoreApplication/BookstoreApplication/Data/AppDbContext.cs
+++ BookstoreApplication/BookstoreApplication/Data/AppDbContext.cs
@@ -10,5 +10,7 @@ namespace BookstoreApplication.Data
         public DbSet<Author> Authors { get; set; }
         public DbSet<Book> Books { get; set; }
         public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<Award> Awards { get; set; }
+        public DbSet<AuthorAward> AuthorAwards { get; set; }
     }
 }
---
+++ BookstoreApplication/BookstoreApplication/Models/AuthorAward.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookstoreApplication.Models
+{
+    public class AuthorAward
+    {
+        public int Id { get; set; }
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+        public int AwardId { get; set; }
+        public Award Award { get; set; }
+
+        [Range(1900, 2100, ErrorMessage = "Year received must be between 1900 and 2100.")]
+        public int YearReceived { get; set; }
+    }
+}
